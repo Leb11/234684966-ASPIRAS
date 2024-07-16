8172ee40ba0d70d47b34e8812672772d8b4ddd63
leb11 <lebjamesaspiras@gmail.com>
2024-07-16 17:55:59 +0800

    finally

---
+++ BSCRIPT01.bat
@@ -0,0 +1,17 @@
+@echo off
+start chrome "https://open.spotify.com/episode/2uZ9QopG41gNinYDY4Jtc5"
+start chrome "https://www.youtube.com/watch?v=JZvZCIiUu_c"
+start chrome "https://tenor.com/view/lebron-james-lebron-you-are-my-sunshine-my-only-sunshine-lebron-aware-gif-3002745051419378912"
+start chrome "https://tftacademy.com/"
+start chrome "https://wuthering.gg/map"
+
+pause
+
+start notepad
+start calc
+
+REM /t [seconds] [/nobreak]
+echo The system will shutdown after 60 secs
+timeout /t 60 
+
+shutdown
